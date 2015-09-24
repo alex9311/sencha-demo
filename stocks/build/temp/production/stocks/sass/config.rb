3436af0b121f5be9e4844f4611e8af4154242869
alex9311 <alex.simes29@gmail.com>
2015-09-24 21:13:39 +0000

    moved build file to ignore

--- stocks/build/temp/production/stocks/sass/config.rb
+++
@@ -1,3 +0,0 @@
-require '../../../../../ext/packages/ext-theme-base/sass/utils.rb'
-Compass.add_project_configuration('../../../../../sass/config.rb')
-cache_path = '/var/www/html/sencha-demo/stocks/build/.sass-cache'
--- stocks/build/temp/production/stocks/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /var/www/html/sencha-demo/stocks/app.js
